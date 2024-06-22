95d7d6c68a8167411a6ca10c95201d3f96ae7418
YEAHx4 <developerstarlight@gmail.com>
2024-06-23 01:35:29 +0900

    Bot precense playing EFT

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { ActivityType, Client, Events, GatewayIntentBits } from "discord.js";
 import { BOT_TOKEN, validateConfig } from "./config";
+import { startWebServer } from "./web";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+  client.user!!.setPresence({
+    activities: [
+      {
+        name: `Escape from Tarkov`,
+        type: ActivityType.Playing,
+      },
+    ],
+    status: "online",
+  });
 });
 
 validateConfig();
 client.login(BOT_TOKEN);
+startWebServer();
