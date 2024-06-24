53ed9e5217c8dffc71dff407d6a08fa6a9ee2ece
YEAHx4 <developerstarlight@gmail.com>
2024-06-24 13:21:30 +0900

    Auto refresh slash commands

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,8 @@ config();
 const logger = getLogger("config");
 logger.info(".env configuration loaded!");
 
-export const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN || "";
+export const BOT_TOKEN = process.env.BOT_TOKEN || "";
+export const BOT_CLIENT_ID = process.env.BOT_CLIENT_ID || "";
 export const WEB_PORT = process.env.PORT || 8080;
 export const LOG_DIR = process.env.LOG_DIR || "./logs";
 
@@ -13,10 +14,14 @@ export const validateConfig = () => {
   logger.debug("Validating configuration...");
 
   if (!BOT_TOKEN) {
-    const err = new Error("DISCORD_BOT_TOKEN is required in .env file");
-    logger.error("DISCORD_BOT_TOKEN is required in .env file", err);
+    const err = new Error("BOT_TOKEN is required in .env file");
+    logger.error("BOT_TOKEN is required in .env file", err);
+    throw err;
+  } else if (!BOT_CLIENT_ID) {
+    const err = new Error("BOT_CLIENT_ID is required in .env file");
+    logger.error("BOT_CLIENT_ID is required in .env file", err);
     throw err;
   }
 
-  logger.info("Configuration is valid!");
+  logger.info("Configuration validaion ended successfully!");
 };
